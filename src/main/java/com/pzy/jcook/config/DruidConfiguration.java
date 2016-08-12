b54c3a7fc5dcb891419774daf6411e3cec3618b4
zhaoyang0501 <zhaoyang0501@126.com>
2016-08-12 18:08:19 +0800

    1

---
+++ src/main/java/com/pzy/jcook/config/DruidConfiguration.java
@@ -0,0 +1,55 @@
+package com.pzy.jcook.config;
+
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+
+
+public class DruidConfiguration {
+	
+    public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new WebStatFilter());
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        return filterRegistrationBean;
+    }
+    
+    public ServletRegistrationBean druidServlet() {
+    	 ServletRegistrationBean reg = new ServletRegistrationBean();
+         reg.setServlet(new StatViewServlet());
+         reg.addUrlMappings("/druid/index");
+         //reg.addInitParameter("allow", "127.0.0.1");
+         //reg.addInitParameter("deny","");
+         reg.addInitParameter("loginUsername", "admin");
+         reg.addInitParameter("loginPassword", "admin");
+         return reg;
+    }
+    
+    public DataSource druidDataSource(@Value("${spring.datasource.driverClassName}") String driver,
+                                      @Value("${spring.datasource.url}") String url,
+                                      @Value("${spring.datasource.username}") String username,
+                                      @Value("${spring.datasource.password}") String password) {
+        DruidDataSource druidDataSource = new DruidDataSource();
+        druidDataSource.setDriverClassName(driver);
+        druidDataSource.setUrl(url);
+        druidDataSource.setUsername(username);
+        druidDataSource.setPassword(password);
+        try {
+            druidDataSource.setFilters("stat, wall");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return druidDataSource;
+    }
+}
