503f31fc2a2dd9f8f2f3cd368975287b676d635a
zhaoyang0501 <zhaoyang0501@126.com>
2016-09-08 17:55:57 +0800

    bug111 333

--- src/main/java/com/pzy/jcook/workflow/web/FileUploadController.java
+++ src/main/java/com/pzy/jcook/workflow/web/FileUploadController.java
@@ -2,11 +2,15 @@ package com.pzy.jcook.workflow.web;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.lang3.time.DateUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,15 +32,38 @@ public class FileUploadController {
 	
 	 @Value("${spring.jsp.uploadpath}")
 	 private String uploadpath;
-	
+	 
+	 /***
+	  * 被运行的文件类型
+	  */
+	 private List<String>  allowTypes = new ArrayList<String>(){
+		private static final long serialVersionUID = -7478928976664848255L;
+		{
+			 add("png");
+			 add("jpg");
+			 add("jpeg");
+			 add("gif");
+			 add("bmp");
+			 add("mp4");
+			 add("mp3");
+		 }
+	 };
+	 
+	 
 	 @RequestMapping(value="/upload", method=RequestMethod.POST)
 	 @ResponseBody
 	  public  Response handleFileUpload( @RequestParam("file") MultipartFile file,HttpServletRequest request) throws Exception, IOException{
 	        if (!file.isEmpty()) {
 	        	String filename =  file.getOriginalFilename();  
-                String filePath = request.getSession().getServletContext().getRealPath("/") + "upload/"+ file.getOriginalFilename();  
+	        	String fileType = StringUtils.getFilenameExtension(filename);
+	        	
+	        	if(!allowTypes.contains(fileType.toLowerCase()))
+	        		return new FailedResponse("不被允许的文件类型");
+	        	
+	        	String newfilename = System.currentTimeMillis()+"."+ StringUtils.getFilenameExtension(filename);
+                String filePath = request.getSession().getServletContext().getRealPath("/") + "upload/"+ newfilename;  
                 file.transferTo(new File(filePath));  
-                return new ObjectResponse<FileUploadDTO>(new FileUploadDTO(filename,filename));
+                return new ObjectResponse<FileUploadDTO>(new FileUploadDTO(filename,newfilename));
 	        }
 	        else
 	        	return new FailedResponse("上传失败");
